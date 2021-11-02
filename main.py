1ff82786291d3a07358ac3921c07dd80b9fc76ac
Alexey Voylukov <dwg_dwg@mail.ru>
2021-11-02 18:50:49 +0300

    Second version. Many def))

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 import glob
 
-# from pprint import pprint
+def read_file(list_files,data_in_files):
+    num_f = 0
+    for id_files in list_files:
+        num_f += 1
+        num_line = 0
+        with open(id_files, encoding='utf-8') as file:
+            data = []
+            for line_text in file:
+                num_line += 1
+                data.append(line_text.strip() + '\n')
+            data_in_files[id_files] = data, num_line
+    return num_f
+
+def init_mass(num):
+    s_file = [[0, 0, 0]]
+    for id in range(num - 1):
+        s_file.append([0, 0, 0])
+    return s_file
+
+def write_file(name, s_file):
+    with open(name, 'wt', encoding='utf-8') as file:
+        for id in s_file:
+            file.write(id[1] + '\n')
+            file.write(str(id[0]) + '\n')
+            file.write("".join(id[2]))
+
+def sort_file(ovr_r, s_file):
+    n = 0
+    for id in ovr_r:
+        if n == 0:
+            s_file[0][0] = ovr_r[id][1]
+            s_file[0][1] = id
+            s_file[0][2] = ovr_r[id][0]
+        else:
+            flag = 0
+            for i in range(n):
+                if ovr_r[id][1] < s_file[i][0] and flag == 0:
+                    s_file.insert(i, [ovr_r[id][1], id, ovr_r[id][0]])
+                    s_file.pop(-1)
+                    flag = 1
+                elif flag == 0 and i + 1 == n:
+                    s_file[i + 1][0] = ovr_r[id][1]
+                    s_file[i + 1][1] = id
+                    s_file[i + 1][2] = ovr_r[id][0]
+        n += 1
+
 
 file_list=glob.glob("*.txt")
-
 ovr_read = {}
-num_file = 0
-for id_files in file_list:
-    num_file += 1
-    num_line = 0
-    with open(id_files, encoding='utf-8') as file:
-        data = []
-        for line_text in file:
-            num_line += 1
-            data.append(line_text.strip()+'\n')
-        ovr_read[id_files] = data, num_line
-
-
-save_file = [[0,0,0]]
-for id in range(num_file-1):
-    save_file.append([0,0,0])
-
-n = 0
-for id in ovr_read:
-    if n == 0:
-        save_file[0][0] = ovr_read[id][1]
-        save_file[0][1] = id
-        save_file[0][2] = ovr_read[id][0]
-    else:
-        flag = 0
-        for i in range(n):
-            if ovr_read[id][1] < save_file[i][0] and flag == 0:
-                save_file.insert(i,[ovr_read[id][1], id, ovr_read[id][0]])
-                save_file.pop(-1)
-                flag = 1
-            elif flag == 0 and i+1 == n:
-                save_file[i+1][0] = ovr_read[id][1]
-                save_file[i+1][1] = id
-                save_file[i+1][2] = ovr_read[id][0]
-    n += 1
-
-with open('result.tx2','wt', encoding='utf-8') as file:
-    for id in save_file:
-        file.write(id[1]+'\n')
-        file.write(str(id[0])+'\n')
-        file.write("".join(id[2]))
+num_file = read_file(file_list, ovr_read)
+save_file = init_mass(num_file)
+sort_file(ovr_read, save_file)
+write_file('result.tx2', save_file)
